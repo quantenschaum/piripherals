8d44165386596ea884a2ebc1f5331ba5016dad28
quantenschaum <software@louisenhof2.de>
2017-10-15 15:43:20 +0200

    keywords

--- piripherals/__version__.py
+++ piripherals/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'piripherals'
 __description__ = 'a collection of classes to interact with peripherals for the RaspberryPi'
 __url__ = 'https://quantenschaum.github.io/piripherals/'
-__version__ = '0.1b4'
+__version__ = '0.1a1'
 __author__ = 'quantenschaum'
 __author_email__ = 'software@louisenhof2.de'
 __license__ = 'GPLv3'
